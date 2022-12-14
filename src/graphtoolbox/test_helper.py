d6dea2c98cc067f1da1abb123cef944478fe6878
Jannick Stuby <mail@jannick-stuby.com>
2022-12-14 15:06:29 +0100

    Adding testhelper and autobuild

---
+++ src/graphtoolbox/test_helper.py
@@ -0,0 +1,16 @@
+class TestHelper:
+    """
+    This is a Helper class for testing documentation builds
+    """
+
+    def __init__(self):
+        """
+        Constructor for TestHelper
+        """
+        pass
+
+    def test_one(self):
+        """
+        This is a function comment in TestHelper
+        """
+        pass
