781402e08e5792f53531e4f1196c0747f07b88d9
chrishaxalot <45143330+chrishaxalot@users.noreply.github.com>
2023-05-01 12:00:23 +0200

    saving before switching to edge information

--- examples/random_walk/clean_for_abgabe.py
+++ examples/random_walk/clean_for_abgabe.py
@@ -8,7 +8,9 @@ from tqdm import tqdm
 import random
 
 
-feat_to_include = range(9)
+vertex_feat_to_include = range(9)
+edge_feat_to_include = range(3)
+
 
 def graph_dict_to_nx_graph(
     graph_dict: dict
@@ -46,37 +48,40 @@ def get_shortest_paths(graph):
 def substitute_nodes_with_features(shortest_path, graph):
     path_str = []
     for node_id in shortest_path:
-        for j, feat in enumerate(feat_to_include):
+        for j, feat in enumerate(vertex_feat_to_include):
             path_str.append(f'{j}_{graph.nodes[node_id]["feature"][feat]}')
+        path_str.append('EOW')
     path_str = ' '.join(path_str)
     return path_str
 
 
-def write_paths_to_file(dataset):
+def write_paths_to_file(dataset, dataset_name):
     start_line = 1
     line = 1
-    with open('rows.cor', 'w') as f_rows, open('shortest_paths.cor', 'w') as f_paths:
+    with open(f'data/rows.cor', 'w') as f_rows, open(f'data/shortest_paths.cor', 'w') as f_paths:
         for i, (graph_dict, label) in enumerate(dataset):
             graph = graph_dict_to_nx_graph(graph_dict)
             shortest_paths = get_shortest_paths(graph)
             for shortest_path in shortest_paths:
                 path_str = substitute_nodes_with_features(shortest_path, graph)
-                f_paths.write(path_str + " STOP ")
+                f_paths.write(path_str + "\n")
                 line += 1
             f_paths.write("\n")
             f_rows.write(str(start_line) + " " + str(line-1) + '\n')
             start_line = line
 
+
 results = []
 
-for _ in tqdm(range(100)):
+for _ in tqdm(range(1)):
     dataset_name = 'ogbg-molfreesolv'
     dataset = GraphPropPredDataset(name=dataset_name)
     split_idx = dataset.get_idx_split()
 
-    write_paths_to_file(dataset)
+    write_paths_to_file(dataset, dataset_name)
 
-    model = Doc2Vec(corpus_file='shortest_paths.cor', window=5*len(feat_to_include))
+    model = Doc2Vec(corpus_file='data/shortest_paths.cor',
+                    window=5*len(vertex_feat_to_include))
 
     train_X = np.array([model.dv[x] for x in split_idx['train']])
     train_y = np.array([dataset[x][1] for x in split_idx['train']]).ravel()
@@ -88,7 +93,6 @@ for _ in tqdm(range(100)):
     valid_y = np.array([dataset[x][1] for x in split_idx['valid']]).ravel()
 
     train_X.shape, train_y.shape
-
 
     reg = RandomForestRegressor()
     reg.fit(train_X, train_y)
