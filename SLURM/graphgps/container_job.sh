a42d432d6032626f945a173ee72d983623e74535
st160888@uni-stuttgart.de <st_st160888@uc2n994.localdomain>
2023-05-04 13:35:38 +0200

    updated container_job.sh to working version, needs conda_gps folder in home dir

--- SLURM/graphgps/container_job.sh
+++ SLURM/graphgps/container_job.sh
@@ -5,12 +5,11 @@
 #SBATCH --container-image=./ubuntu+latest.sqsh
 #SBATCH --container-mounts=/etc/slurm/task_prolog:/etc/slurm/task_prolog,/scratch:/scratch,./graph-embedding:/GraphGPS,./conda_gps:/GraphGPS/gps
 #SBATCH --container-writable
-#SBATCH --output=out.txt
-#SBATCH --error=err.txt
+#SBATCH --container-remap-root
 
-apt update && apt upgrade -y
+apt-get update && apt-get upgrade -y
 cd /root
-apt install wget -y
+apt-get install wget -y
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 pwd
 ls -al
@@ -21,4 +20,4 @@ conda update -y conda
 conda init bash
 cd /GraphGPS
 conda activate ./gps
-python main.py --cfg configs/GPS/ogbg-molpcba-GPS.yaml  wandb.use False
+#python main.py --cfg configs/GPS/ogbg-molpcba-GPS.yaml  wandb.use False
